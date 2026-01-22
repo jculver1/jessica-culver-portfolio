ed1865e05439194a63427c5d50247e447bd93062
Jessica culver <jculver@MacBookAir.lan>
2026-01-21 20:27:12 -0500

    fixed deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
   plugins: [react()],
   base: "/jessica-culver-portfolio"
 }));
